51ed68c847641fa96ecc12fa693d9f69067c7c5d
polina-fer <polina.rykova@fer.unizg.hr>
2025-05-27 16:02:54 +0200

    load reference

--- decompress_hirgc.cpp
+++ decompress_hirgc.cpp
@@ -1,15 +1,22 @@
 #include <cstring>
+#include <fstream>
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+const int MAX_SEQ_LENGTH = 1 << 28;
 
 /// Struct for reference and target file names
 struct InputFileNames {
   string reference_file;
-  string target_file;
+  string compressed_target_file;
 };
 
-void showHelpMessage(string reason) {
+vector<char> ref_seq;
+vector<char> target_seq;
+
+void show_help_message(string reason) {
   /**
    * Displays an error message along with usage instructions.
    * Used when the user provides invalid arguments.
@@ -20,6 +27,53 @@ void showHelpMessage(string reason) {
        << endl;
 }
 
+void initialize_structures() {
+  /**
+   * Initializes all the data structures used in the decompression process.
+   */
+  ref_seq.reserve(MAX_SEQ_LENGTH);
+  target_seq.reserve(MAX_SEQ_LENGTH);
+}
+
+void load_and_clean_reference(const string& filename, vector<char>& sequence) {
+  /**
+   * Load and clean reference genome sequence
+   * removes any non-ACGT characters, converts to uppercase
+   */
+
+  ifstream file(filename);
+  if (!file) {
+    throw runtime_error("Cannot open file: " + filename);
+  }
+
+  string line;
+  while (getline(file, line)) {
+    if (line[0] == '>') {  // Skip header line
+      continue;
+    };
+
+    if (line.empty()) {  // Skip empty lines
+      continue;
+    }
+
+    for (char c : line) {
+      c = toupper(c);  // Convert to uppercase
+      if (c == 'A' || c == 'C' || c == 'G' || c == 'T') {
+        sequence.push_back(c);
+        cout << c;
+      }
+    }
+  }
+}
+
+void cleanup() {
+  /**
+   * Cleans up and releases all allocated memory
+   */
+  ref_seq.clear();
+  target_seq.clear();
+}
+
 int main(int argc, char* argv[]) {
   /**
    * Main function for compressing files using HIRGC algorithm.
@@ -27,12 +81,12 @@ int main(int argc, char* argv[]) {
 
   // Check if passed arguments are valid
   if (argc != 5) {
-    showHelpMessage("Invalid number of arguments.");
+    show_help_message("Invalid number of arguments.");
     return 1;
   }
 
   if (strcmp(argv[1], "-r") != 0 || strcmp(argv[3], "-t") != 0) {
-    showHelpMessage("Invalid arguments.");
+    show_help_message("Invalid arguments.");
     return 1;
   }
 
@@ -40,12 +94,15 @@ int main(int argc, char* argv[]) {
   InputFileNames input_file_names;
 
   input_file_names.reference_file = argv[2];
-  input_file_names.target_file = argv[4];
+  input_file_names.compressed_target_file = argv[4];
 
-  cout << "Reference: " << input_file_names.reference_file << endl;
-  cout << "Target: " << input_file_names.target_file << endl;
+  initialize_structures();
+
+  load_and_clean_reference(input_file_names.reference_file, ref_seq);
 
   cout << "Decompressing..." << endl;
+
+  cleanup();
 
   return 0;
 }
